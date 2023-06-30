1e3d75cb6fb5526e2589a3c7979065a3c7d45ffd
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-06-30 10:56:00 +0100

    Created models controller and view Models: Movie, Director, TvShow. Created controller movies and added the index action Added the resources only index for the movies Added the searchbar in the index.html.erb for the movies view and also the display of the results Seeded the database with some instances of movies directors and tv_shows

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_094833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "directors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "synopsis"
+    t.bigint "director_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+  end
+
+  create_table "tv_shows", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "synopsis"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "movies", "directors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+url = "https://gist.githubusercontent.com/ssaunier/be9a933b64116e2422176aab7528473e/raw/d1e1b06e25616771fddf44bf066765f518b0655d/imdb.yml"
+sample = YAML.load(URI.parse(url).read)
+
+puts "Creating directors..."
+directors = {} # slug => Director
+sample["directors"].each do |director|
+  directors[director["slug"]] = Director.create!(director.slice("first_name", "last_name"))
+end
+
+puts "Creating movies..."
+sample["movies"].each do |movie|
+  Movie.create!(movie.slice("title", "year", "synopsis").merge(director: directors[movie["director_slug"]]))
+end
+
+puts "Creating TV shows..."
+sample["tv_shows"].each { |tv_show| TvShow.create!(tv_show) }
+puts "Done."
